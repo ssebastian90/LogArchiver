e18fb82b95abd6dddd414d6efd672b8ab55df67f
samihaesie <sebastian.amihaesie@gmail.com>
2023-09-29 10:29:52 +0300

    error handling in case the log file is used by another process

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import re
 import json
 import shutil
+import logging
 from datetime import datetime, timedelta
 from dateutil import parser
 
@@ -14,6 +15,8 @@ class LogArchiver:
         self.file_paths = data['path']
         self.days_to_subtract = data['days_to_subtract']
 
+        logging.basicConfig(filename='logarchiver.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
     def path_exists(self, path):
         return os.path.exists(path)
 
@@ -23,22 +26,29 @@ class LogArchiver:
         current_date = datetime.now().date()
 
         for file in files:
-            found_date = date_filter.search(file)
+            try:
 
-            if found_date:
-                found_date = found_date.group()
-                date = parser.parse(found_date).date()
+                found_date = date_filter.search(file)
 
-                if date < current_date:
-                    shutil.move(os.path.join(path, file), os.path.join(self.archive_path, file))
-                    print(f'moved file:{file} to archive folder')
+                if found_date:
+                    found_date = found_date.group()
+                    date = parser.parse(found_date).date()
+
+                    if date < current_date:
+                        shutil.move(os.path.join(path, file), os.path.join(self.archive_path, file))
+                        logging.info(f'moved file:{file} to archive folder')
+
+            except Exception as e:
+                logging.error(f'Exception found on {file}: {e}')
+            
+            logging.info('archive process complete!')
 
     def delete_older_log_files(self):
         files = os.listdir(self.archive_path)
         date_filter = re.compile(r'\d{8}|\d{2,4}-\d{2}-\d{2,4}')
         end_date = (datetime.now() - timedelta(days=self.days_to_subtract)).date()
 
-        print(f'deleting files older than {end_date}')
+        logging.info(f'deleting files older than {end_date}')
 
         for file in files:
             found_date = date_filter.search(file)
@@ -49,17 +59,17 @@ class LogArchiver:
 
                 if date <= end_date:
                     os.remove(os.path.join(self.archive_path, file))
-                    print(f'file was deleted {os.path.join(self.archive_path, file)}')
+                    logging.info(f'file was deleted {os.path.join(self.archive_path, file)}')
 
     def archive_logs(self):
         for path in self.file_paths:
             self.archive_path = os.path.join(path, 'archive')
 
-            print(f'archive logs on {path}')
+            logging.info(f'archive logs on {path}')
 
             if not self.path_exists(self.archive_path):
                 os.mkdir(self.archive_path)
-                print(f'archive path was created {self.archive_path}')
+                logging.info(f'archive path was created {self.archive_path}')
 
             self.move_logs_to_archive(path)
             self.delete_older_log_files()
